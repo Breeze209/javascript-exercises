d7bbe58e64a6dfa82ae19e17aef12915832e065c
Breeze209 <236219025+Breeze209@users.noreply.github.com>
2025-10-21 09:10:24 +0200

    Solve js exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let inCelsius = (fah -32) * (5 / 9);
+  inCelsius = Math.round(inCelsius * 10) / 10;
+  return inCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let inFahrenheit = (cel * (9 / 5)) +32;
+  inFahrenheit = Math.round(inFahrenheit *10) / 10;
+  return inFahrenheit;
 };
 
 // Do not edit below this line
