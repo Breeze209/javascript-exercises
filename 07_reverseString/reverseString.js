66fa47e77731488920c4d2b25ccd83191939238a
Breeze209 <236219025+Breeze209@users.noreply.github.com>
2025-10-21 07:53:09 +0200

    Solve js exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split('');
+    string = arr.reverse().join('');
+    return string
 };
 
 // Do not edit below this line
