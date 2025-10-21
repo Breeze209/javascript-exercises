2d6b7995ae90978b8056e8340eb7656e978410a3
Breeze209 <236219025+Breeze209@users.noreply.github.com>
2025-10-21 08:30:09 +0200

    Solve js exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (Math.floor(a) !== a || Math.floor(b) !== b) {
+        return 'ERROR';
+    } else if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    let solution = 0;
+
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            solution += i;
+        }
+    } else if (a > b) {
+        for (let i = b; i <= a; i++) {
+            solution += i;
+        }
+    }
+    
+    return solution;
 };
 
 // Do not edit below this line
